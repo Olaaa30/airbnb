bf316d01d03db5fc1111b94eba350a63594f0818
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2022-08-25 21:10:03 +0100

    Push files to github

---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function Navbar(){
+    return(
+        <nav className="nav">
+            <img className="airbnb" src="../images/airbnb-logo.png"/>
+        </nav>
+    )
+}
